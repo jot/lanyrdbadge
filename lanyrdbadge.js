4b9a2253004e104b014c3da96283c6afe7f6f3fa
Jonathan Markwell <jonathan.markwell@gmail.com>
2011-09-29 13:31:30 -0300

    Forked for use with venues.

--- lanyrdbadge.js
+++ lanyrdbadge.js
@@ -4,26 +4,27 @@ lanyrdbadge = function(){
     l = document.getElementById('lanyrd');
     if(l && l.nodeName.toLowerCase() === 'a'){
       l.innerHTML += ' <span>(Loading&hellip)</span>';
-      var user = l.getAttribute('href');
+      var venue = l.getAttribute('href');
       var url = 'http://query.yahooapis.com/v1/public/yql?'+
                 'q=select%20*%20from%20html%20where%20url%3D%22'+
-                 encodeURIComponent(user)+
-                '%22%20and%20xpath%3D%22%2F%2Fh2%5Bcontains(.%2C\''+
-                'Future\')%5D%2F..%22&diagnostics=true&format=xml'+
-                '&callback=lanyrdbadge.seed';
+                 encodeURIComponent(venue)+
+                '%22%20and%20xpath%3D%22%2F%2F*%5Bcontains(%40class%2C\'vevent\')%5D%22'+
+                '&diagnostics=true&format=xml&callback=lanyrdbadge.seed';
       var s = document.createElement('script');
       s.setAttribute('src',url);
       document.getElementsByTagName('head')[0].appendChild(s);
     }
   }
   function seed(o){
-    var res = o.results[0];
-    res = res.replace(/class="split"/,'id="lanyrd"');
-    res = res.replace(/speaking at/,'');
-    res = res.replace(/href="/,'href="http://lanyrd.com');
-    res = res.replace(/src="/gi,'src="http://lanyrd.com'); 
-    l.parentNode.innerHTML = res;
-  }
+		var events = '';
+		for (i=0;i< o.results.length;i++){
+    	var res = o.results[i];
+    	res = res.replace(/href="/,'href="http://lanyrd.com');
+    	res = res.replace(/src="/gi,'src="http://lanyrd.com');
+			events += res;
+  	}
+  	l.parentNode.innerHTML = '<ol class="lanyrd events">'+events+'</ol>';
+	}
   return {seed:seed,init:init}
 }();
 lanyrdbadge.init();
